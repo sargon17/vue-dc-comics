e08eab86d37afa4d9671d70c9610cfe89edf45fa
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-06 18:17:32 +0200

    main section done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HeaderComponent />
+    <MainContentSection />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import MainContentSection from "./components/MainContentSection.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    MainContentSection,
   },
 };
 </script>
---
+++ src/components/MainContentSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <h2>Conten goes here</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainContentSection",
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  height: 100px;
+  background: #1c1c1c;
+  color: white;
+}
+</style>
