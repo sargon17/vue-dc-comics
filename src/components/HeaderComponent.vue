715e5c1f478d39c095cf2cd1e41226fe83e3cab8
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-06 17:16:55 +0200

    init

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header></header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 100px;
+  background: #f543b5;
+}
+</style>
