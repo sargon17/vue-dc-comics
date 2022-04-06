26177f34af028fc00b5f835655ff29e31b8f975b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-06 17:49:06 +0200

    imported files data done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,67 @@
 <template>
   <div>
-    <header></header>
+    <header class="header">
+      <div class="header--content">
+        <div class="header__logo-wrapper">
+          <img src="../assets/dc-logo.png" alt="dc logo" />
+        </div>
+        <nav class="header__nav">
+          <ul class="header__nav-list">
+            <li
+              v-for="{ title, id } in navigationData"
+              :key="id"
+              class="header__nav-item"
+            >
+              <a href="#" class="header__nav-link">
+                <span class="header__nav-link-text">{{ title }}</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
+import navigationData from "../data/navigationData.js";
+
 export default {
   name: "HeaderComponent",
+  data() {
+    return {
+      navigationData,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-header {
+.header {
   width: 100%;
   height: 100px;
-  background: #f543b5;
+  background: #ffffff;
+
+  &--content {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+  }
+
+  &__nav {
+    &-list {
+      display: flex;
+      align-items: center;
+      list-style: none;
+    }
+    &-item {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+    }
+  }
 }
 </style>
---
+++ src/data/navigationData.js
@@ -0,0 +1,44 @@
+const navigationData = [
+  {
+    title: "Characters",
+    id: "characters",
+  },
+  {
+    title: "Comics",
+    id: "comics",
+  },
+  {
+    title: "Movies",
+    id: "movies",
+  },
+  {
+    title: "Tv",
+    id: "tv",
+  },
+  {
+    title: "Games",
+    id: "games",
+  },
+  {
+    title: "Collectibles",
+    id: "collectibles",
+  },
+  {
+    title: "Videos",
+    id: "videos",
+  },
+  {
+    title: "Fans",
+    id: "fans",
+  },
+  {
+    title: "News",
+    id: "news",
+  },
+  {
+    title: "Shop",
+    id: "shop",
+  },
+];
+
+export default navigationData;
